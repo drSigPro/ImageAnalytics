eef5dd287db52111b232ef3fa3ae44277cb67a91
Mahesh Raveendranatha Panicker <160823293+drSigPro@users.noreply.github.com>
2024-03-02 12:50:55 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Main",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Image/Video Analytics Demo!")
+
+st.markdown(
+    """
+    The demo consists of analysis of significance of spatial and intensity resolution, contrast, histograms, smoothing and gradients, and then feature extraction
+    
+    Please select appropriate tab from left pane
+    
+    """
+)
+
